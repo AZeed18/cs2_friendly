62509c5f595e11fbae83ef3b8e3e0665c09319fd
Ahmed Al Zaydi <44865525+AZeed18@users.noreply.github.com>
2026-02-16 21:25:00 +0200

    Automate gameinfo.gi modification

--- cs2friendly.sh
+++ cs2friendly.sh
@@ -2,13 +2,20 @@ cd "$(dirname "$0")"
 
 if [ "$(crc32 steam.inf)" != "$(cat friendly.crc32 2>/dev/null)" ]; then
 	crc32 steam.inf > friendly.crc32
+
 	cp pak01_dir.vpk pak01_dir.vpk.bak
+	mv gameinfo.gi gameinfo.gi.bak
 	mv ../bin/win64/vpk.signatures ../bin/win64/vpk.signatures.bak
 
 	echo "Installing mod..."
+
 	cat friendly/friendly.txt | sed -n 's/\.\//--remove-file /p' | xargs -n200 "C:\Program Files\VPKEdit\vpkeditcli.exe" pak01_dir.vpk >/dev/null
-	echo "Finished removing assets references"
-	echo -n 'Open gameinfo.gi and add "Game csgo/friendly" beside any other "Game" then press Enter...'; read
+	echo "Removed assets references"
+
+	sed -E 's/^(\s*Game\s)(csgo\s*)$/\1csgo\/friendly\n\1\2/' gameinfo.gi.bak > gameinfo.gi
+	echo "Added mod to gameinfo.gi"
+
+	echo -n 'Press Enter to continue...'; read
 fi
 
 explorer steam://launch/730
